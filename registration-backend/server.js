3d593c0582a3995ac9510055c8e570ff64fbaf98
neemapssree <145227095+neemapssree@users.noreply.github.com>
2023-12-19 23:40:36 +0400

    Added files

---
+++ registration-backend/server.js
@@ -0,0 +1,32 @@
+// server.js or your route file
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const RegisterModel = require('./models/Register');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+app.post("/register", (req, res) => {
+    const { name, email, password } = req.body;
+    RegisterModel.findOne({ email: email })
+        .then(user => {
+            if (user) {
+                res.status(400).json({ message: 'User already exists' });
+            } else {
+                RegisterModel.create({ name: name, email: email, password: password })
+                    .then(result => res.status(201).json(result))
+                    .catch(err => res.status(500).json({ message: 'Error creating user', error: err }));
+            }
+        })
+        .catch(err => res.status(500).json({ message: 'Server error', error: err }));
+});
+
+
+const port = 4000;
+app.listen(port, () => {
+    console.log('Server is running on port', port);
+});
